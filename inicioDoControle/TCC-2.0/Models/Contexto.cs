962a86c203b971eed9e61c36faedbca44e125c6c
jureguelenda <davidfreitassouza081@gmail.com>
2022-11-08 15:06:26 -0300

    Projeto no Core

---
+++ inicioDoControle/TCC-2.0/Models/Contexto.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TCC_2._0.Models
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+
+        }
+        public DbSet<ENTRADA> ENTRADA { get; set; }
+    }
+}
